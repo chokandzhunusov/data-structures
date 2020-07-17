75300d34a2710b8d32aaa2d73b75e290ebb93fba
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-07-17 11:03:43 +0600

    WIP: params

--- params.c
+++ params.c
@@ -1,12 +1,12 @@
 /*
- * Parameters:
- *              Pass by Value
- *              Pass by Address
- *              Pass by Reference(c++)
+ * Parameters: Pass by Value, Pass by Address, Pass by Reference(c++)
+ *             Passing `array` is only pass by addresses
+ *             Passing `structure`
  */
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 void swapByValue(int x, int y)
@@ -15,7 +15,7 @@ void swapByValue(int x, int y)
     temp = x;
     x = y;
     y = temp;
-}
+};
 
 
 void swapByAddress(int *x, int *y)
@@ -28,19 +28,82 @@ void swapByAddress(int *x, int *y)
     temp = *x;
     *x = *y;
     *y = temp;
+};
+
+
+void f(int A[], int n)
+{
+    /*
+     * A: an array, passed by address
+     * n: size of array, passed by value
+     */
+    int i;
+    for (i=0; i<n; i++)
+        printf("\n%d", A[i]);
+};
+
+
+int * f2(int n)
+{
+    /*
+     * n: size of array to be allocated in HEAP memory
+     */
+     int *p;
+     p = (int *)malloc(n*sizeof(int));
+     return (p);
+};
+
+
+struct Rectangle
+{
+    int length;
+    int breadth;
+};
+
+
+int area(struct Rectangle r1)
+{
+    /*
+     * Separate object will be created fro r1 with values length, breadth
+     */
+     return r1.length * r1.breadth;
+};
+
+
+void changeLength()
+{
+
 }
 
 
 int main()
 {
+    // eg of passing params by value == Pass by Value
     int a, b;
     a = 10;
     b = 20;
-    // eg of passing params by value == Pass by Value
     swapByValue(a, b);
     printf("%d %d", a, b);
+
 
     // eg of passing params by address == Pass by Address
     swapByAddress(&a, &b);
     printf("\n%d %d", a, b);
+
+
+    // eg of passing `arrays` to function
+    int A[5] = {2, 3, 4, 5, 6};
+    f(A, 5);
+
+
+    // eg of returning an `array` in function
+    int *B;
+    B = f2(5);
+
+
+    // eg of passing `structure` to function == Call by Value
+    struct Rectangle r = {10, 5};
+    printf("%d", area(r));
+
+    // eg of passing `structure` to function == Call by Address
 }
