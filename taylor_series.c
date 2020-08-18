18b6f8681d78924794cd3fe44a08702210f2cbb4
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-08-18 16:08:36 +0600

    Add: Taylor Series Horners Rule and iterative methods

--- taylor_series.c
+++ taylor_series.c
@@ -32,7 +32,34 @@ double taylor_series(int x, int n)
 }
 
 
+double taylor_series_horners_rule(int x, int n)
+{
+    static double s;
+    if(n==0 || n<0)
+        return s;
+    s = 1 + x*s / n;
+    return taylor_series_horners_rule(x, n-1);
+}
+
+
+double taylor_series_iterative(int x, int n)
+{
+    double s=1;
+    int i;
+    double num=1;
+    double den=1;
+
+    for(i=1; i<=n; i++)
+    {
+        num *= x;
+        den *= i;
+        s += num/den;
+    }
+    return s;
+}
+
+
 int main()
 {
-    printf("%1f \n", taylor_series(4, 15));
+
 }
