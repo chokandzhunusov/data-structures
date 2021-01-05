8dcbb88688dddc6a6d8dd4441692ea831731b0c0
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-01-05 10:39:13 +0600

    MODIFY: view

--- Section 6 | Arrays Representation/78_declaration.c
+++ Section 6 | Arrays Representation/78_declaration.c
@@ -9,23 +9,25 @@ int A3[5] = {1, 2};      // First two indices will be set and others will be set
 int A4[] = {1, 2, 3, 4, 5};   // Array of size of elements will be created
 
 
-
 int main()
 {
     // Array element access #1
-    printf("%d", A2[3]);
+    // printf("%d", A2[3]);
 
     // Traversing an Array and accessing it's elements
-    for(int i=0; i<5; i++)
-    {
-        printf("%d", A2[i]);
-    }
+    // for(int i=0; i<5; i++)
+        // printf("%d", A2[i]);
 
     // Array element access #2
-    printf("%d", 3[A2]);
+    // printf("%d", 3[A2]);
 
     // Array element access #3
-    printf("%d", *(A2 + 3));
+    // printf("%d", *(A2 + 3));
+
+    // TODO
+    // Print addresses of array `A`
+    for(int j=0; j<5; j++)
+        printf("%u \n", &A[j]);
 
     return 0;
 }
