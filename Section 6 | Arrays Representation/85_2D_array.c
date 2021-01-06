ecae4cbf1d715732926ba8138a1dde67754e9151
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-01-06 10:13:54 +0600

    ADD: 2D arrays in C

---
+++ Section 6 | Arrays Representation/85_2D_array.c
@@ -0,0 +1,34 @@
+/*
+ *  ======================
+ *  2D arrays.
+ *  ======================
+ *  2D Arrays will be created in Stack memory.
+ *  If you will make pointers, then it will be array of arrays.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    // TODO
+    // Everything in Stack
+    // Initialize 2D array with len (3,4)
+
+    // Partially in Stack, partially in HEAP
+    // Initialize an Array pointer of size 3
+    // For pointers variables created above create associated arrays of size 4 in HEAP
+    // For pointer index 1, for Array in HEAP index 2 set value = 15
+
+    // Everything in HEAP
+    // Initialize double pointer in Stack
+    // Point variable created above to new array in HEAP with size 3
+    // For each indice created above create new arrays in HEAP if size 4 each
+    
+    // Try to access each value of array
+
+    return 0;
+}
