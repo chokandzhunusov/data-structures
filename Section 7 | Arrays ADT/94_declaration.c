b377cf84a5db9d412865dd667818f5e6591a6914
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-11 10:23:25 +0600

    ADD: finalize declaration of and Arrays ADT

--- Section 7 | Arrays ADT/94_declaration.c
+++ Section 7 | Arrays ADT/94_declaration.c
@@ -7,30 +7,41 @@ struct Array {
     int *A;     // integer type pointer
     int size;
     int length;
-}
+};
 
 
 void Display(struct Array arr)
 {
     int i;
-    printf("\n Elements are \n")
-    for(i=0, i<arr.length; i++)
+    printf("\n Elements are \n");
+    for(i=0; i<arr.length; i++)
         printf("%d ", arr.A[i]);
 }
 
 int main() {
+    // Initialize an Array and t integers
     struct Array arr;
     int n, i;
-    printf("Enter size of and array");
+
+    // Prompt msg for user to enter the int to be size of an array
+    printf("Enter size of and array: ");
     scanf("%d", &arr.size);
+
+    // Set the size of an array with num taken from input of user
     arr.A = (int *)malloc(arr.size * sizeof(int));
-    arr.length = 0
-    printf("Enter number of numbers")
+    arr.length = 0;
+
+    // Ask user how many numbers will be inserted
+    printf("Enter number of numbers: ");
     scanf("%d", &n);
-    printf("Enter all elements\n")
-    for(i=0,i<n,i++)
-        scanf("%d", &arr.A[i];);
+
+    // Start taking numbers to be set in Array
+    printf("Enter all elements\n");
+    for(i=0; i<n; i++)
+        scanf("%d", &arr.A[i]);
     arr.length = n;
+
+    // Show all elements of an Array using function Display() created above
     Display(arr);
     return 0;
 }
