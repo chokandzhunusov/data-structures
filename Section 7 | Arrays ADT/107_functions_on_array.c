1e7fc217a1d8b28890e1861ad6348a0b4d73eb99
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-13 11:08:37 +0600

    WIP: functions on an Arrays

---
+++ Section 7 | Arrays ADT/107_functions_on_array.c
@@ -0,0 +1,33 @@
+/*
+ *  ======================
+ *  Get(), Set(), Max(), Min(), Avrg() functions on array
+ *  ======================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+/*
+ - For unsorted list we have to check all elements
+ Max():
+    max = A[0];                     ====> 1
+    for(i=1; i<length; i++)         ====> n
+    {
+        if(A[i]>max)                ====> n-1
+            max = A[i];
+    }
+    return max;                     ====> 1
+
+                                    ====> f(n) = 2n +1
+                                               = O(n)
+*/
+
+
+
+int main()
+{
+    return 0;
+}
