af3f1231011db96317dfedbe3d2ad008ecec7681
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-17 09:54:19 +0600

    ADD: manipulations with sorting an array

--- Section 7 | Arrays ADT/111_check_if_array_sorted.c
+++ Section 7 | Arrays ADT/111_check_if_array_sorted.c
@@ -28,20 +28,47 @@ void Display(struct Array arr)
 }
 
 
+void Swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+
 void InsertToSortedList(struct Array *arr, int x)
 {
-    int i = arr->length-1;
-    while(x < arr->A[i])
+    int i=arr->length-1;
+    if(arr->size==arr->length)
+        return;
+    while(i>=0 && arr->A[i] > x)
     {
         arr->A[i+1] = arr->A[i];
         i--;
     }
     arr->A[i+1] = x;
+    arr->length++;
+}
+
+void REarrange(struct Array *arr)
+{
+    int i = 0;
+    int j = arr->length-1;
+    while (i<j)
+    {
+        while(arr->A[i]<0)
+            i++;
+        while(arr->A[j]>=0)
+            j--;
+        if(i<j)
+            Swap(&arr->A[i], &arr->A[j]);
+    }
 }
 
 int main()
 {
-    struct Array arr={{2, 3, 14, 5, 6}, 10, 5};
+    struct Array arr={{2, 3, 5, 6, -3, 15, -1}, 10, 7};
     Display(arr);
     return 0;
 }
