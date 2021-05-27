8be45419f250ff5422947d5c4b9523a12994dbb7
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-27 11:10:23 +0600

    WIP: counting vowels and consonants in a sentence

---
+++ Section 8 | Strings/133_counting_vowels_and_consonents.c
@@ -0,0 +1,23 @@
+/*
+ *  ==============================
+ *  Counting Vowels and Consonants
+ *  ==============================
+ */
+
+
+#include <stdio.h>
+
+
+int main()
+ {
+    char A[] = "How are you";
+    int i, vcount=0;
+    for (i=0; A[i]!='\0'; i++)
+    {
+
+    }
+
+    printf("Num of Vowels is: %d, consonants: %d", A);
+
+    return 0;
+ }
