d6817ca38aca2b6f2f887d2d7033a08fd2ed4880
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-28 09:30:02 +0600

    ADD: examples of counting alphabet chars and words in sentence

--- Section 8 | Strings/133_counting_vowels_and_consonents.c
+++ Section 8 | Strings/133_counting_vowels_and_consonents.c
@@ -8,16 +8,39 @@
 #include <stdio.h>
 
 
+void CountVowelsAndConsonants(char A[] )
+{
+    int i, vcount=0, ccount=0;
+    for (i=0; A[i]!='\0'; i++)
+    {
+        if(A[i]=='A' || A[i]=='E' || A[i]=='I' || A[i]=='O' || A[i]=='U' ||
+           A[i]=='a' || A[i]=='e' || A[i]=='i' || A[i]=='o' || A[i]=='u')
+            vcount += 1;
+        else if((A[i]>=65 && A[i]<=90) || (A[i]>=97 && A[i]<=122))
+            ccount += 1;
+    }
+
+    printf("Num of Vowels is: %d, consonants: %d", vcount, ccount);
+}
+
+
+void CountNumOfWords(char A[] )
+{
+    int i, words=1;
+    for (i=0; A[i]!='\0'; i++)
+    {
+        if(A[i] == ' ' && A[i-1] != ' ')
+            words += 1;
+    }
+
+    printf("Num of words in sentence is: %d", words);
+}
+
+
 int main()
  {
     char A[] = "How are you";
-    int i, vcount=0;
-    for (i=0; A[i]!='\0'; i++)
-    {
-
-    }
-
-    printf("Num of Vowels is: %d, consonants: %d", A);
-
+    CountVowelsAndConsonants(A);
+    CountNumOfWords(A);
     return 0;
  }
