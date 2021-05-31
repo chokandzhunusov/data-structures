0d794011b647c2f7986eaaa535a285330a34797c
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-05-31 09:52:52 +0600

    ADD: checking of string for Palindrome

--- Section 8 | Strings/136_comparing_srt_palindrome.c
+++ Section 8 | Strings/136_comparing_srt_palindrome.c
@@ -1,7 +1,7 @@
 /*
- *  ==================
- *  Reversing a string
- *  ==================
+ *  =================================
+ *  Comparing a string for Palindrome
+ *  =================================
  */
 
 
@@ -26,6 +26,54 @@ void CaseSensitiveComparisonOfWords(char *word1, char *word2)
 }
 
 
+int IsPalindromeVer1(char *word)
+{
+    int i, j;
+
+    for (i=0; word[i]!='\0'; i++){}
+    i -= 1;
+
+    char word_reverse_copy[i];
+
+    for (j=0; i>=0; j++, i--)
+        word_reverse_copy[j] = word[i];
+
+    word_reverse_copy[j] = '\0';
+
+    for (i=0; i<=j; i++)
+    {
+        if (word[i] != word_reverse_copy[i])
+        {
+            printf("The word `%s` is not a palindrome!\n", word);
+            return -1;
+        }
+    }
+
+    printf("Yep, the word `%s` is a palindrome!", word);
+    return 1;
+}
+
+
+int IsPalindromeVer2(char *word)
+{
+    int i, j;
+    for (i=0; word[i]!='\0'; i++){}
+    i -= 1;
+
+    for (j=0; j!=i; j++, i--)
+    {
+        if (word[i] != word[j])
+        {
+            printf("The word `%s` is not a palindrome!\n", word);
+            return -1;
+        }
+    }
+
+    printf("Yep, the word `%s` is a palindrome!", word);
+    return 1;
+}
+
+
 int Palindrome(char *word)
 {
     return 0;
@@ -37,6 +85,9 @@ int main()
     char word1[] = "painter";
     char word2[] = "painting";
     CaseSensitiveComparisonOfWords(word1, word2);
+
+    char word[] = "madam";
+    IsPalindromeVer2(word);
 
     return 0;
  }
