71019586ca167702ac98faf3ae1d2c1cb522bb6b
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-04 10:13:59 +0600

    ADD: permutation of a string

--- Section 8 | Strings/140_permutation_of_string.c
+++ Section 8 | Strings/140_permutation_of_string.c
@@ -22,16 +22,62 @@
 #include <stdio.h>
 
 
-int Perm(char S, int k)
+void Perm(char *word, int k)
 {
+    static char result[10];
+    static int flag[10] = {0,};
 
+    if (word[k] == '\0')
+    {
+        result[k] = '\0';
+        printf("%s\n", result);
+    }
+    else
+    {
+        for (int i=0; word[i]!='\0'; i++)
+        {
+            if (flag[i] == 0)
+            {
+                flag[i] = 1;
+                result[i] = word[k];
+                Perm(word, k+1);
+                flag[i] = 0;
+            }
+        }
+    }
 }
 
 
+void swap(char *word, int a, int b)
+{
+    char temp;
+    temp = word[a];
+    word[a] = word[b];
+    word[b] = temp;
+}
+
+
+void PermMethod2(char *word, int l, int h)
+{
+    int i;
+
+    if (l == h)
+        printf("%s\n", word);
+    else
+    {
+        for (i=l; i<=h; i++)
+        {
+            swap(word, l, i);
+            PermMethod2(word, l+1, h);
+            swap(word, l, i);
+        }
+    }
+}
+
 
 int main()
- {
-    char S[] = "ABC";
-    Perm(S, 0);
-    return 0;
- }
+{
+   char S[] = "ABC";
+   PermMethod2(S, 0, 2);
+   return 0;
+}
