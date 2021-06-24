9c6ad7a8a8ef10837ede6e73e52efa16eba70e15
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-06-24 10:52:41 +0600

    ADD: reason of using linked lists

---
+++ Section 11 | Linked List/170_why_linked_list.c
@@ -0,0 +1,27 @@
+/*
+ *  =====================================
+ *  Why we need Dynamic Data Sturctures
+ *  =====================================
+ *  1. Problem with arrays
+        - Fixed size created and can not be changed during run time
+        - int A[10]
+            => can not be >10,
+            => also if there are just 2 elements to store causes waste of space
+        - creation:
+            => in stack: int A[5]
+            => in heap(pointer): int *p = new A[5]
+ *  2. Difference between Array and LinkedList
+        - element space and pointer space together we call as - NODE
+ *
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    return 0;
+}
