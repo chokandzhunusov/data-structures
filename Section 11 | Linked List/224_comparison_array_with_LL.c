fb1a2fc9f8a79fbd6c897e4044319561509528a3
chokandzhunusov <chokan.dzhunusov@gmail.com>
2021-07-17 09:26:51 +0600

    ADD: comparison of Array and Linked List

---
+++ Section 11 | Linked List/224_comparison_array_with_LL.c
@@ -0,0 +1,35 @@
+/*
+ *  ===================================
+ *  Comparison of Array with Linked List
+ *  ===================================
+ *  There are 2 physical data structures, using them we implement other DS:
+    1. Array
+        - can be stored in Stack as well in Heap
+        - fixed size
+        - can be accessed in constant time any item
+        - faster access 1step if in Stack, 2 steps if in Heap
+        - insertion/deletion to left=O(n), to right=O(1)
+        - Linear Search algorithm will take O(n)
+        - Binary Search algorithm will take O(logn)
+        - Most of sorting is designed for this
+
+    2. Linked List
+        - in Heap
+        - size can be changes
+        - have to loop to access item
+        - insertion/deletion to left=O(1), to right=O(n)
+        - Linear Search algorithm will take O(n)
+        - Binary Search algorithm will take O(nlogn)
+        - less sorting approaches than array
+
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    return 0;
+}
