3d854bd2b2978c2ef68de873d83e9394981b5274
Chokan <chokan.dzhunusov@gmail.com>
2021-08-18 09:35:37 +0600

    ADD: convertion of expression from `infix` to `postfix` with additional right to left associativity

--- Section 13 | Stack/246_infix_to_postfix.c
+++ Section 13 | Stack/246_infix_to_postfix.c
@@ -58,7 +58,7 @@ void display()
 
 int isOperand(char x)
 {
-    if (x=='+' || x=='-' || x=='*' || x=='/')
+    if (x=='+' || x=='-' || x=='*' || x=='/' || x=='^' || x=='(' || x==')')
         return 0;
     return 1;
 }
@@ -71,6 +71,7 @@ int precedence(char x)
         return 2;
     return 0;
 }
+
 
 char * convert(char *infix)
 {
@@ -100,12 +101,79 @@ char * convert(char *infix)
 }
 
 
+int outStackPrecedence(char x)
+{
+    if (x=='+' || x=='-')
+        return 1;
+    else if (x=='*' || x=='/')
+        return 3;
+    else if (x=='^') // right to left associative operator(decreasing)
+        return 6;
+    else if (x=='(')
+        return 7;
+    return 0;
+}
+
+
+int inStackPrecedence(char x)
+{
+    if (x=='+' || x=='-')
+        return 2;
+    else if (x=='*' || x=='/')
+        return 4;
+    else if (x=='^')  // right to left associative operator(decreasing)
+        return 5;
+    else if (x=='(')
+        return 0;
+    return 0;
+}
+
+
+// Converting expression that involves more operands and that have right to left association
+char * convert2(char *infix)
+{
+    int i=0, j=0;
+    int len = strlen(infix)+2; // +1 is for `\0` and +1 is for hash '#'
+    char *postfix;
+    postfix = (char *)malloc(sizeof(char)*len);
+
+    while (infix[i] != '\0')
+    {
+        if (isOperand(infix[i]))
+            postfix[j++] = infix[i++];
+        else
+        {
+            if (outStackPrecedence(infix[i]) > inStackPrecedence(top->data))
+                push(infix[i++]);
+            else if (outStackPrecedence(infix[i]) == inStackPrecedence(top->data))
+            {
+                pop();
+                i++;
+            }
+            else
+                postfix[j++] = pop();
+        }
+    }
+
+    while (top!=NULL)
+        postfix[j++] = pop();
+    postfix[j] = '\0';
+
+    return postfix;
+}
+
+
 int main()
 {
     char *infix="a+b*c-d/e";
     push('#');
     char *postfix = convert(infix);
-    printf("%s ", postfix);
+    printf("Given `infix` form of expression: %s, the `postfix` form of it is: %s \n", infix, postfix);
+
+    char *infix2="((a+b)*c)-d^e^f";
+    push('#');
+    char *postfix2 = convert2(infix2);
+    printf("Given `infix` form of expression: %s, the `postfix` form of it is: %s \n", infix2, postfix2);
     return 0;
 }
 
