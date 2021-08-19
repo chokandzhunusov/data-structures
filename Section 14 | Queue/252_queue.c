f23e4af2e4f130adb62f48f60aa19435525c1c61
Chokan <chokan.dzhunusov@gmail.com>
2021-08-19 10:49:40 +0600

    ADD: intro to Queues

---
+++ Section 14 | Queue/252_queue.c
@@ -0,0 +1,27 @@
+/*
+    Queue:
+        - Logical data structure
+        - Can be implemented using 2 physical data structures:
+            1. Array
+            2. Linked List
+        - First IN, first OUT
+        - front(deletion) ... rear(insertion)
+    Queue ADT:
+        - Data:
+            1. Space for storing elements
+            2. Front - for deletion
+            3. Rear - for insertion
+        - Operations:
+            1. enqueue(x)
+            2. dequeue()
+            3. isEmpty()
+            4. isFull()
+            5. first()
+            6. last()
+*/
+
+
+int main()
+{
+    return 0;
+}
