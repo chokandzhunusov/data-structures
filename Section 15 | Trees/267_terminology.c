b78c82724f92aa64233880d8ac11eed1e4562222
Chokan <chokan.dzhunusov@gmail.com>
2021-11-23 13:00:19 +0300

    ADD: terminology used in Trees DS

---
+++ Section 15 | Trees/267_terminology.c
@@ -0,0 +1,21 @@
+/*
+Terminology:
+    - Tree - collection of Nodes and Edges
+    - One node is taken as the “root” node. And divided into “disjoint subsets”, each subset is “tree” or “subtree”
+    - n nodes => n-1 edges
+    - Siblings - nodes that are bind to same parent
+    - Descendants - nodes that are belongs to from up to down
+    - Ancestors - nodes that are prev to node till “root”
+    - Degree of node - how many direct children node have
+    - External/Leaf/Terminal nodes are those who have zero degree
+    - Level - the depth from “root” to leaf, starts from 1
+    - Height - count edges, starts from 0
+    - Forest - collection of trees
+    - Binary Tree: Tree of degree 2 (max)
+*/
+
+
+int main()
+{
+
+}
